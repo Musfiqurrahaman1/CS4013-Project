5674f5acf329f4fcf49fd673eff6c128cdc6f02d
Musfiqurrahaman1 <musfiqurrahaman12@gmail.com>
2024-12-04 03:05:13 +0000

    Payslip.java

--- Payslip.java
+++ Payslip.java
@@ -1,25 +1,42 @@
 public class Payslip {
     private Employee employee;
-    private double grossSalary;
+    private double grossPay;
     private double totalDeductions;
-    private double netSalary;
+    private double incomeTax;
+    private double healthInsurance;
+    private double unionFee;
+    private double prsi;
+    private double usc;
 
     // Constructor
-    public Payslip(Employee employee, double grossSalary, double totalDeductions) {
+    public Payslip(Employee employee, double grossPay, double totalDeductions,
+                   double incomeTax, double healthInsurance, double unionFee,
+                   double prsi, double usc) {
         this.employee = employee;
-        this.grossSalary = grossSalary;
+        this.grossPay = grossPay;
         this.totalDeductions = totalDeductions;
-        this.netSalary = grossSalary - totalDeductions;
+        this.incomeTax = incomeTax;
+        this.healthInsurance = healthInsurance;
+        this.unionFee = unionFee;
+        this.prsi = prsi;
+        this.usc = usc;
     }
 
-    // Display payslip
-    public void displayPayslip() {
-        System.out.println("Payslip for: " + employee.getName());
+
+    public void displayPayslip() {  // this displays all the nessesary info needed on a payslip
+        System.out.println("\n=== Payslip ===");
+        System.out.println("Employee Name: " + employee.getName());
         System.out.println("Position: " + employee.getPosition());
-        System.out.println("Years Worked: " + employee.getYearsWorked());
-        System.out.println("Gross Salary: €" + grossSalary);
+        System.out.println("Employee Type: " + (employee.getHourlyRate() > 0 ? "Part-Time" : "Full-Time"));
+        System.out.println("Gross Pay: €" + grossPay);
+        System.out.println("Income Tax: €" + incomeTax);
+        System.out.println("PRSI: €" + prsi);
+        System.out.println("USC: €" + usc);
+        System.out.println("Health Insurance: €" + healthInsurance);
+        System.out.println("Union Fee: €" + unionFee);
         System.out.println("Total Deductions: €" + totalDeductions);
-        System.out.println("Net Salary: €" + netSalary);
+        System.out.println("Net Pay: €" + (grossPay - totalDeductions));
+        System.out.println("================");
     }
 }
 
